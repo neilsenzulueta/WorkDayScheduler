1a610df100031c215105d01a423f9e1035d876fa
neilsen zulueta <neilzulueta@gmail.com>
2023-10-20 07:28:07 -1000

    added on HTML

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,37 @@
+//current date using day.js
+var currentDay = dayjs().format("dddd, MMMM Do")
+$('#currentDate').text(time);
+
+
+
+
+
+
+
+
+/*user story 
+as an employee with a busy schedule 
+i wanto add important events to a daily planner
+so that i can mange my time effectively
+
+Acceptance criteria 
+Given I am using a daily planer to create a schedule 
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down 
+THEN I am presented with time blocks for standard business hours of 9am to 5pm
+WHEN I view the time blocks for that day 
+THEN each time block is color coded to indicate whether it is in the past present or future 
+WHEN I click into a time block 
+THEN I can enter an event 
+WHEN I click the save button for that time block 
+THEN the text for that event is saved in local storage 
+WHEN I refresh the page 
+THEN the saved events persist 
+
+create a 
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
