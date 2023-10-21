37de67779758fa9b91d2ebe13880e43aa33ef73b
neilsen zulueta <neilzulueta@gmail.com>
2023-10-20 15:35:42 -1000

    updated html and js

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,15 @@
 $(function() {
   $(".saveBtn").click(function (event) {
     var parentId = $(this).parent().attr("id");
-    var getHour = parseInt(parentId.slice(5));
-    var targetDay = dayjs().format("dddd-MMMM-Do" + getHour);
-    var targetHour = dayjs(targetDay);
-    var today = day.js().format("dddd-MMMM-Do HH");
-    var difference = targetHour.diff(today, "hour");
-    
-    var assignment = document.querySelector("#" + parentId + "textarea").ariaValueMax;
-    localStorage.setItem(parent, assignment);
-
+    var assignment = $("#" + parentId + " textarea").val(); // Change to .val() to get the textarea value
+    localStorage.setItem(parentId, assignment);
   });
   $(".time-block").each(function () {
     var parentId = $(this).attr("id");
     var getHour = parseInt(parentId.slice(5));
     var targetDay = dayjs().format("dddd-MMMM-Do" + getHour);
     var targetHour = dayjs(targetDay);
-    var today = day.js().format("dddd-MMMM-Do HH");
+    var today = dayjs().format("dddd-MMMM-Do HH");
     var difference = targetHour.diff(today, "hour");
     console.log(difference)
 
@@ -24,23 +17,17 @@ $(function() {
       $(this).addClass("present");
     }
     if (difference < 0) {
-      $(this).addClass("future");
-    }
-    if (difference > 0) {
-      $(this).addClass("past")
+      $(this).addClass("past");
+    } else {
+      $(this).addClass("future")
     }
   });
   $(".time-block").each(function () {
       var parentId = $(this).attr("id");
-      var getHour = parseInt(parentId.slice(5));
-      var targetDay = dayjs().format("dddd-MMMM-Do " + getHour);
-      var targetHour = dayjs(targetDay);
-      var today = dayjs().format("dddd-MMMM-Do HH");
-      var difference = targetHour.diff(today, "hour");
       var assignment = localStorage.getItem(parentId);
 
-      if(assignment != null) {
-        document.querySelector("#" + parentId + "textarea").value = assignment;
+      if (assignment !== null) {
+        $("#" + parentId + " textarea").val(assignment); // Change to .val() to set the textarea value
       }
   });
 
