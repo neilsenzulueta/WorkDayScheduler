3369fb6f42a83d76a2f81fb556668e1058d05d74
neilsen zulueta <neilzulueta@gmail.com>
2023-10-20 17:30:20 -1000

    added comments js

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,11 @@
+//This is executed when page is fully loaded.
 $(function() {
   $(".saveBtn").click(function (event) {
     var parentId = $(this).parent().attr("id");
     var assignment = $("#" + parentId + " textarea").val();
     localStorage.setItem(parentId, assignment);
   });
+  //A loop through each element with time-block.
   $(".time-block").each(function () {
     var parentId = $(this).attr("id");
     var getHour = parseInt(parentId.slice(5));
@@ -11,7 +13,7 @@ $(function() {
     var targetHour = dayjs(targetDay);
     var today = dayjs().format("YYYY-MM-DD H");
     var difference = targetHour.diff(today, "hour");
-
+  //Past,present,future color coded depending on time difference.
     if (difference === 0) {
       $(this).addClass("present");
     } else if (difference < 0) {
@@ -20,15 +22,16 @@ $(function() {
       $(this).addClass("future")
     }
   });
+  //A loop through each element with time-block
   $(".time-block").each(function () {
       var parentId = $(this).attr("id");
       var assignment = localStorage.getItem(parentId);
-
+      //Display assignment if found in local storage.
       if (assignment !== null) {
         $("#" + parentId + " textarea").val(assignment);
       }
   });
-
+  //Displaying current time and date in "currentDay".
   var time = dayjs().format("dddd, MMMM D, YYYY");
   $("#currentDay").text(time);
 
