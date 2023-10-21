3128a4f9a6870584cf59f2aebf8f2a85b300eb37
neilsen zulueta <neilzulueta@gmail.com>
2023-10-20 14:45:22 -1000

    updated js

--- assets/script.js
+++ assets/script.js
@@ -2,21 +2,39 @@ $(function() {
   $(".saveBtn").click(function (event) {
     var parentId = $(this).parent().attr("id");
     var getHour = parseInt(parentId.slice(5));
-    var targetDay = day.js().format('dddd-MMMM-Do' + getHour);
-    var targetHour = day.js(targetDay);
-    var today = day.js().format('dddd-MMMM-Do HH');
-    var difference = targetHour.diff(today, 'hour');
+    var targetDay = dayjs().format("dddd-MMMM-Do" + getHour);
+    var targetHour = dayjs(targetDay);
+    var today = day.js().format("dddd-MMMM-Do HH");
+    var difference = targetHour.diff(today, "hour");
     
-    var assignment = document.querySelector('#' + parentId + 'textarea').ariaValueMax;
+    var assignment = document.querySelector("#" + parentId + "textarea").ariaValueMax;
     localStorage.setItem(parent, assignment);
 
 });
-}
+  $(".time-block").each(function () {
+    var parentId = $(this).attr("id");
+    var getHour = parseInt(parentId.slice(5));
+    var targetDay = dayjs().format("dddd-MMMM-Do" + getHour);
+    var targetHour = dayjs(targetDay);
+    var today = day.js().format("dddd-MMMM-Do HH");
+    var difference = targetHour.diff(today, "hour");
+    console.log(difference)
 
+    if (difference === 0) {
+      $(this).addClass("present");
+    }
+    if (difference < 0) {
+      $(this).addClass("future");
+    }
+    if (difference > 0) {
+      $(this).addClass("past")
+    }
+}
 )
 //current date using day.js
 var currentDay = dayjs().format("dddd, MMMM Do")
 $('#currentDate').text(time);
+}
 
 /*user story 
 as an employee with a busy schedule 
